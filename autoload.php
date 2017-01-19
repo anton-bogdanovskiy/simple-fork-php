a9d3c21de5db356595d4fb601da6ff04c8de3806
Anton Bogdanovskiy <anton@anchorfree.com>
2017-01-18 16:54:13 -0800

    Fix autoloader

--- autoload.php
+++ autoload.php
@@ -7,9 +7,13 @@
  */
 
 spl_autoload_register(function ($classname) {
-    $dir = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR;
-    if (strstr($classname, "\\Jenner\\SimpleFork\\") === 0) {
-        $file = $dir . basename(str_replace('\\', '/', $classname)) . '.php';
-        if (file_exists($file)) require $file;
+    if (strpos($classname, 'Jenner\\SimpleFork\\') === 0) {
+        $class = str_replace('Jenner\\SimpleFork\\', '', $classname);
+        $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
+        $file  = __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . $class . '.php';
+
+        if (file_exists($file)) {
+            require $file;
+        }
     }
 });
